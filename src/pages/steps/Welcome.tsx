e7efc5f03fcbe84297c7a289cc361a98d07d9892
Linda Dietrich <linda.dietrich@tngtech.com>
2020-12-18 11:43:36 +0100

    change color schema

--- src/pages/steps/Welcome.tsx
+++ src/pages/steps/Welcome.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import {IonButton, IonContent, IonPage} from "@ionic/react";
 import {StepProps} from "./commonSteps";
+import {CheckPosition} from "../../components/CheckPosition";
 
 export const Welcome: React.FC<StepProps> = ({goToNextStep}) => {
 
     return (
         <IonPage>
-            <IonContent fullscreen>
+            <IonContent fullscreen style={{'--background': '#fffaf7'}}>
                 welcome screen
                 <IonButton onClick={goToNextStep}>Es kann los gehen</IonButton>
             </IonContent>
